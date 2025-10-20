f08dbfd5086e5415ff8b9f360d5d47d45beb922d
Sonu Kumar <166662101+rajsonugupta11@users.noreply.github.com>
2025-10-21 00:43:27 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,10 +5,11 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "Bot is running 24/7!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    Thread(target=run).start()
+    t = Thread(target=run)
+    t.start()
